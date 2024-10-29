3a9b29bbfbb8ca74122fd5a5408efb21bafed042
alex <alexmagikov228@gmail.com>
2024-10-29 23:32:59 +0300

    task with tests

--- stack/task/task/main.c
+++ stack/task/task/main.c
@@ -1,6 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "stack.h"
 #include <stdio.h>
 #include <stdbool.h>
+
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 bool testForCreateStack() {
 	Stack* stack = createStack();
@@ -26,14 +32,68 @@ bool testForIsEmpty() {
 	return isEmpty(stack);
 }
 
+int calculateInPostfix(const char* string) {
+	Stack* stack = createStack();
+	for (int i = 0; i < strlen(string); i++) {
+		if (isdigit(string[i])) {
+			push(stack, string[i] - '0');
+		}
+		else {
+			if (!isEmpty(stack)) {
+				int tmp = peek(stack);
+				pop(stack);
+				if (string[i] == '-') {
+					tmp = peek(stack) - tmp;
+				}
+				else if (string[i] == '+') {
+					tmp = peek(stack) + tmp;
+				}
+				else if (string[i] == '/') {
+					if (tmp == 0) {
+						printf("u cant divide by 0 ");
+						exit(0);
+						return -1;
+					}
+					tmp = peek(stack) / tmp;
+				}
+				else if (string[i] == '*') {
+					tmp = peek(stack) * tmp;
+				}
+				pop(stack);
+				push(stack, tmp);
+			}
+		}
+	}
+	if (isEmpty(stack)) {
+		return -1;
+	}
+	else {
+		int result = peek(stack);
+		free(stack);
+		return result;
+	}
+ }
+
+bool testForPostfixCalculatorForNormalValue() {
+	return calculateInPostfix("96-12+*") == 9;
+}
+
+bool testForPostfixCalculatorForBorderValue() {
+	return calculateInPostfix("1") == 1;
+}
+
+bool testForPostfixCalculatorForNULLValue() {
+	return calculateInPostfix(" ") == -1;
+}
+
 void main(void) {
-	if (!testForIsEmpty() || !testForPeek || !testForCreateStack() || !testForPop()) {
+	if (!testForPostfixCalculatorForNULLValue() || !testForPostfixCalculatorForBorderValue() || !testForPostfixCalculatorForNormalValue() || !testForIsEmpty() || !testForPeek || !testForCreateStack() || !testForPop()) {
 		printf("tests are failed");
 		exit(0);
 	}
 	Stack* stack = createStack();
-	push(stack, 1);
-	printf("%d", peek(stack));
-	printf("%d ", peek(stack));
-	free(stack);
+	const char* string[100];
+	printf("input postfix expression ");
+	scanf("%s", &string);
+	printf("result %d", calculateInPostfix(string));
 }
