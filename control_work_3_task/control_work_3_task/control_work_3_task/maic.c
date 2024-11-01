be30467915b1c94bcf47f121ef50124924e9aafe
alex <alexmagikov228@gmail.com>
2024-11-01 16:20:41 +0300

    with tests

--- control_work_3_task/control_work_3_task/control_work_3_task/maic.c
+++ control_work_3_task/control_work_3_task/control_work_3_task/maic.c
@@ -1,15 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main() {
+void printWithoutEqual() {
     FILE* file;
-    char ch;
     file = fopen("file.txt", "r");
     if (file == NULL) {
         perror("Ошибка открытия файла");
         return 1;
     }
+    char ch;
     char last = "";
     while ((ch = fgetc(file)) != EOF) {
         if (ch != last) {
@@ -18,5 +20,44 @@ int main() {
         }
     }
     fclose(file);
-    return 0;
+}
+
+char* printWithoutEqualForTest(char* fileName) {
+    FILE* file;
+    char ch;
+    file = fopen(fileName, "r");
+    if (file == NULL) {
+        perror("Ошибка открытия файла");
+        return 1;
+    }
+    char result[100];
+    char last = "";
+    int index = 0;
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch != last) {
+            result[index] = ch;
+            last = ch;
+            index++;
+        }
+    }
+    result[index] = '\0';
+    fclose(file);
+    return result;
+}
+
+bool testForPrintWithoutEqualForSingleString() {
+    return !strcmp(printWithoutEqualForTest("fileForTestForSingleString.txt"), "afgba");
+}
+
+bool testForPrintWithoutEqualFor2Strings() {
+    return !strcmp(printWithoutEqualForTest("fileForTestFor2Strings.txt"), "chic\nlasd");
+}
+
+int main() {
+    if (!testForPrintWithoutEqualForSingleString() || !testForPrintWithoutEqualFor2Strings()) {
+        printf("tests are failed");
+        return 0;
+    }
+    printf("text from file without equal:\n");
+    printWithoutEqual();
 }
